e61d0567152ad429ab2bee3c88fb33767746eb35
Joe Dickens <dickens.ja@gmail.com>
2019-10-16 17:01:43 -0700

    pass along metric api host

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,10 @@ echo $AWS_CONTAINER_CREDENTIALS_RELATIVE_URI > /root/AWS_CONTAINER_CREDENTIALS_R
 echo $RABBIT_MQ_URL > /root/RABBIT_MQ_URL
 echo $RABBIT_MQ_USERNAME > /root/RABBIT_MQ_USERNAME
 echo $RABBIT_MQ_PASSWORD > /root/RABBIT_MQ_PASSWORD
+echo $METRIC_API_HOST > /root/METRIC_API_HOST
 
 echo $ENVIRONMENT > /root/ENVIRONMENT
-echo "* * * * * root PYTHONPATH=/usr/local/lib/python3.7/site-packages/ AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=`cat /root/AWS_CONTAINER_CREDENTIALS_RELATIVE_URI` RABBIT_MQ_URL=`cat /root/RABBIT_MQ_URL` RABBIT_MQ_USERNAME=`cat /root/RABBIT_MQ_USERNAME` RABBIT_MQ_PASSWORD=`cat /root/RABBIT_MQ_PASSWORD` /usr/local/bin/python /code/app.py >> /var/log/cron.log 2>&1
+echo "* * * * * root PYTHONPATH=/usr/local/lib/python3.7/site-packages/ AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=`cat /root/AWS_CONTAINER_CREDENTIALS_RELATIVE_URI` RABBIT_MQ_URL=`cat /root/RABBIT_MQ_URL` RABBIT_MQ_USERNAME=`cat /root/RABBIT_MQ_USERNAME` RABBIT_MQ_PASSWORD=`cat /root/RABBIT_MQ_PASSWORD` METRIC_API_HOST=`cat /root/METRIC_API_HOST` /usr/local/bin/python /code/app.py >> /var/log/cron.log 2>&1
 " >> /etc/cron.d/report-rabbitmq-status
 
 # Hand off to the CMD
